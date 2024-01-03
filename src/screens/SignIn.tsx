3877bc717f7cde48612a5c87812899359db9e90e
Diego-Batista <diegobtistadev@gmail.com>
2024-01-03 14:32:26 -0300

    Finalizing SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Center, Heading, Image, Text, VStack } from "native-base";
+import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
 
 import BackgroundImage from "@assets/background.png";
 import LogoSvg from '@assets/logo.svg';
@@ -7,39 +7,45 @@ import { Input } from "@components/Input";
 
 export function SignIn() {
     return (
-        <VStack flex={1} bg='gray.700' px={10}>
-            <Image
-                source={BackgroundImage}
-                alt="Pessoas treinando"
-                resizeMode="contain"
-                position='absolute'
-            />
-
-            <Center my={24}>
-                <LogoSvg />
-                <Text color="gray.100" fontSize='sm'>
-                    Treine sua mente e seu corpo
-                </Text>
-            </Center>
-
-            <Center>
-                <Heading color='gray.100' fontSize='xl' fontFamily='heading' mb={6}>
-                    Acesse sua conta
-                </Heading>
-
-                <Input 
-                    placeholder="E-mail"
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                />
-                <Input 
-                    placeholder="Senha"
-                    secureTextEntry
+        <ScrollView contentContainerStyle={{ flexGrow: 1}} showsVerticalScrollIndicator={false}>
+            <VStack flex={1} bg='gray.700' px={10} pb={16}>
+                <Image
+                    source={BackgroundImage}
+                    alt="Pessoas treinando"
+                    resizeMode="contain"
+                    position='absolute'
                 />
 
-                <Button title="Acessar" />
-                <Button title="Criar conta" variant='outline'/>
-            </Center>
-        </VStack>
+                <Center my={24}>
+                    <LogoSvg />
+                    <Text color="gray.100" fontSize='sm'>
+                        Treine sua mente e seu corpo
+                    </Text>
+                </Center>
+
+                <Center>
+                    <Heading color='gray.100' fontSize='xl' fontFamily='heading' mb={6}>
+                        Acesse sua conta
+                    </Heading>
+
+                    <Input 
+                        placeholder="E-mail"
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                    />
+                    <Input 
+                        placeholder="Senha"
+                        secureTextEntry
+                    />
+
+                    <Button title="Acessar" />
+                </Center>
+
+                <Center mt={24}>
+                    <Text color='gray.100' fontSize='sm' mb={3} fontFamily='body'>Ainda não tem acesso?</Text>
+                    <Button title="Criar conta" variant='outline'/>
+                </Center>
+            </VStack>
+        </ScrollView>
     )
 }
