09cb58182fa7dc1753bb73a7883a88e33db2e44d
Diego-Batista <diegobtistadev@gmail.com>
2024-01-13 08:51:32 -0300

    Styling the HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,11 +2,13 @@ import { HStack, Heading, Text, VStack } from "native-base";
 
 export function HomeHeader() {
     return (
-        <HStack>
+        <HStack pt={16} pb={5} px={8} alignItems='center'>
             <VStack>
-                <Text color='gray.100'>Olá</Text>
+                <Text color='gray.100' fontSize='md'>
+                    Olá
+                </Text>
 
-                <Heading color='gray.100'>
+                <Heading color='gray.100' fontSize='md'>
                     Diego
                 </Heading>
             </VStack>
