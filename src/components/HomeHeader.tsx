8f5a62a8603948cec171dbae759f2c3189e66fb8
Diego-Batista <diegobtistadev@gmail.com>
2024-01-13 09:02:44 -0300

    UserPhoto component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,15 @@
 import { HStack, Heading, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
     return (
         <HStack pt={16} pb={5} px={8} alignItems='center'>
+            <UserPhoto 
+                source={{ uri: 'https://github.com/Diego-Batista.png'}}
+                size={16}
+                alt="Image do usúario"
+                mr={4}
+            />
             <VStack>
                 <Text color='gray.100' fontSize='md'>
                     Olá
