e3128453795ce4b99a9670f2af811aa5f0b238ed
Diego-Batista <diegobtistadev@gmail.com>
2024-01-14 08:54:12 -0300

    Logout button

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,4 +1,6 @@
-import { HStack, Heading, Text, VStack } from "native-base";
+import { MaterialIcons } from '@expo/vector-icons';
+import { HStack, Heading, Icon, Text, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
 import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
@@ -19,6 +21,15 @@ export function HomeHeader() {
                     Diego
                 </Heading>
             </VStack>
+
+            <TouchableOpacity>
+                <Icon
+                    as={MaterialIcons}
+                    name="logout"
+                    color='gray.200'
+                    size={7}
+                />
+            </TouchableOpacity>
 
         </HStack>
     )
