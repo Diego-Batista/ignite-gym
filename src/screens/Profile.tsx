2a666035906f17ac552fc81804771d82fe799e89
Diego-Batista <diegobtistadev@gmail.com>
2024-01-24 13:14:27 -0300

    Using toast messages

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -5,9 +5,9 @@ import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import * as FileSystem from 'expo-file-system';
 import * as ImagePicker from "expo-image-picker";
-import { Box, Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { Box, Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from "native-base";
 import { useState } from "react";
-import { Alert, TouchableOpacity } from "react-native";
+import { TouchableOpacity } from "react-native";
 
 const PHOTO_SIZE = 33
 
@@ -15,7 +15,7 @@ export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false)
     const [userPhoto, setUserPhoto] = useState('https://github.com/Diego-Batista.png')
 
-    
+    const toast = useToast()
 
     async function handlePhotoSelected() {
         setPhotoIsLoading(true)
@@ -35,7 +35,11 @@ export function Profile() {
                 const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
 
                 if(photoInfo.exists && (photoInfo.size / 1024 / 1024 > 5)) {
-                    return Alert.alert('Essa imagem é muito grande. Escolha uma de até 5MB.')
+                    return toast.show({
+                        title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+                        placement: 'top',
+                        bgColor: 'red.500'
+                    })
                 }
                 setUserPhoto(photoSelected.assets[0].uri) 
             }
