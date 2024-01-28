b0983001c8741f89a52638f180827fe589c8d421
Diego-Batista <diegobtistadev@gmail.com>
2024-01-28 12:38:27 -0300

    Adjusting Heading

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -92,7 +92,7 @@ export function Profile() {
                 </Center>
 
                 <VStack px={10} mt={12} mb={9}>
-                    <Heading color='gray.200' fontSize='md' mb={2} mt={14}>
+                    <Heading color='gray.200' fontSize='md' mb={2} mt={14} fontFamily='heading'>
                         Alterar senha
                     </Heading>
 
