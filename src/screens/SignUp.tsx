a0f0f90eaf773724a41028ab2a2e8c690475f1e0
Diego-Batista <diegobtistadev@gmail.com>
2024-01-31 13:11:09 -0300

    Obtendo resposta do back-end

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -34,8 +34,17 @@ export function SignUp() {
         navigation.goBack()
     }
 
-    async function handleSignUp({ name, email, password, password_confirm }: FormDataProps){
-       
+    function handleSignUp({ name, email, password }: FormDataProps){
+       fetch('http://192.168.0.178:3333/users', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({name, email, password})
+       })
+       .then(response => response.json())
+       .then(data => console.log(data))
     }
 
     return (
