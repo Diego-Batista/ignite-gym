eda926d53acc66bf052fc57a68a87b1134712d3f
Diego-Batista <diegobtistadev@gmail.com>
2024-02-28 14:39:17 -0300

    Placing requests in the queue

--- src/services/api.ts
+++ src/services/api.ts
@@ -17,6 +17,9 @@ export const api = axios.create({
     baseURL: 'http://192.168.1.5:3333'
 }) as APIInstanceProps;
 
+let failedQueued: Array<PromiseType> = [];
+let isRefreshing = false;
+
 api.registerInterceptTokenManager = singOut => {
     const interceptTokenManager = api.interceptors.response.use((response) => response, async (requestError) => {
       if(requestError.response?.status === 401) {
@@ -28,7 +31,27 @@ api.registerInterceptTokenManager = singOut => {
             return Promise.reject(requestError)
           }
 
+          const originalRequestConfig = requestError.config;
+
+        if(isRefreshing) {
+          return new Promise((resolve, reject) => {
+            failedQueued.push({
+              onSuccess: (token: string) => { 
+                originalRequestConfig.headers = { 'Authorization': `Bearer ${token}` };
+                resolve(api(originalRequestConfig));
+              },
+              onFailure: (error: AxiosError) => {
+                reject(error)
+              },
+            })
+          })
+        }
+
+        isRefreshing = true
+
       }
+
+      singOut();
     }
       
       
